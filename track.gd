acef7cdc1c1dec406f7b2d22e9d7ff9f83d1f137
Tomasz Nowakowski <toalak@gmail.com>
2025-03-19 20:44:33 +0100

    Building a straight road

--- track.gd
+++ track.gd
@@ -3,9 +3,13 @@ extends Node2D
 
 @export var road_width: int
 @export var offroad_lane_width = 200
+var color_dark = Color.BEIGE
+var color_light = Color.ANTIQUE_WHITE
 var lanes = 3
 var segment_length = 200
+var segments_amount = 500
 var rumble_length = 3
+var segments = []
 var track_length
 
 var screen_size = Vector2.ZERO
@@ -14,3 +18,23 @@ var screen_size = Vector2.ZERO
 func _ready():
 	screen_size = get_viewport_rect().size
 	road_width = screen_size.x - 200
+
+
+func reset_road():
+	segments = []
+
+	for i in segments_amount:
+		segments.append(
+			{
+				index = i,
+				p1 = {world = {z = i * segment_length}, camera = {}, screen = {}},
+				p2 = {world = {z = (i + 1) * segment_length}, camera = {}, screen = {}},
+				color = color_dark if floori(float(i)/rumble_length)%2 == 0 else color_light
+			}
+		)
+
+	track_length = segments.size() * segment_length
+
+
+func find_segment(z):
+	return segments[floori(z / segment_length) % segments.size()]
